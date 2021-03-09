46fe3161119c4c5f4c75e8ee80df6af30b528187
Ilham Fairuzaman <ilhamfairuz58@gmail.com>
2021-03-09 17:29:25 +0700

    Penyelesaian Soal 1

--- Soal 1.py
+++ Soal 1.py
@@ -1,48 +1,28 @@
 # Menghitung luas persegi panjang
-def luas_ppanjang():
-    panjang = float(input("Masukkan panjang: "))
-    lebar = float(input("Masukkan lebar: "))
-    rumus_luas = panjang * lebar
-    print("Luas persegi panjang =",rumus_luas)
-    exit()
-
-luas_ppanjang()
+panjang = float(input("Masukkan panjang: "))
+lebar = float(input("Masukkan lebar: "))
+luas_pp = panjang * lebar
+print(luas_pp)
 
 # Menghitung luas lingkaran
-def luas_lingkaran():
-    jari_jari = float(input("Masukkan jari-jari: "))
-    rumus_luas = 3.14 * jari_jari**2
-    print("Luas lingkaran =",rumus_luas)
-    exit()
-
-luas_lingkaran()
+r = float(input("Masukkan jari jari lingkaran: "))
+luas_lingkaran = 3.14 * r**2
+print(luas_lingkaran)
 
 # Menghitung luas kubus
-def luas_kubus():
-    sisi = float(input("Masukkan panjang rusuk kubus: "))
-    rumus_luas = sisi **3
-    print("Luas kubus",rumus_luas)
-    exit()
-
-luas_kubus()
+sisi = float(input("Masukkan panjang rusuk kubus: "))
+luas_kubus = 6 * sisi **2
+print(sisi)
 
 # Program Konversi suhu celcius ke fahrenheit
-def konversicf():
-    celcius = float(input("Masukkan suhu dalam Celcius: "))
-    rumus = (9/5 * celcius) + 32
-    print(celcius, "derajat celcius sama dengan", rumus, "derajat fahrenheit")
-    exit()
-
-konversicf()
+celcius = float(input("Masukkan suhu dalam Celcius: "))
+konversicf = (9/5 * celcius) + 32
+print(konversicf)
 
 # Program Konversi suhu reamur ke kelvin
-def konversirk():
-    reamur = float(input("Masukkan suhu dalam reamur: "))
-    rumus = (5/4 * reamur) + 273
-    print(reamur, "derajat reamur sama dengan", rumus, "derajat kelvin")
-    exit()
-
-konversirk()
+reamur = float(input("Masukkan suhu dalam reamur: "))
+konversirk = (5/4 * reamur) + 273
+print(konversirk)
 
 
 
